dbc6a6358caab138dc3b3ea914e0509a64bb9ebe
SimretAbebe <simretabebe24@gmail.com>
2026-01-30 12:25:35 +0300

    adding some webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,75 @@
 const path = require("path");
-const webpack = require("webpack"); // Webpack core module
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  // ENTRY POINT: Where webpack starts bundling
-  entry: "./src/index.js",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-  // OUTPUT: Where bundled files will be generated
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-  },
+  return {
+    entry: "./src/index.js",
 
-  // MODE: Development mode for hot reloading and debugging
-  mode: "development",
-
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.[contenthash].js",
+      clean: true,
+      publicPath: "/",
     },
-    port: 3003, // Keep this port to avoid conflicts
-    hot: true,
-    open: true,
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/, // Match .js and .jsx files
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-            ],
-            plugins: [
-              "@emotion/babel-plugin", // Re-enable Emotion CSS-in-JS optimizations
-            ],
+    mode: isProduction ? "production" : "development",
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      port: 3003,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+              ],
+              plugins: ["@emotion/babel-plugin"],
+            },
           },
         },
-      },
-      // Removed the CSS loader rule for PostCSS/Tailwind
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name][ext]",
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[name][ext]",
+          },
         },
-      },
+      ],
+    },
+
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+      }),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(
+          isProduction ? "production" : "development",
+        ),
+        "process.env.API_BASE_URL": JSON.stringify(
+          process.env.API_BASE_URL || "https://jsonplaceholder.typicode.com",
+        ),
+      }),
     ],
-  },
-
-  // RESOLVE: How webpack resolves module imports
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(
-        process.env.NODE_ENV || "development"
-      ),
-      "process.env.API_BASE_URL": JSON.stringify(
-        process.env.API_BASE_URL || "https://jsonplaceholder.typicode.com"
-      ),
-    }),
-  ],
+  };
 };
